dc50103a926b72d3fa621487f9a9d410ccbc4030
zer0231 <pranay_zer0@protonmail.com>
2026-03-14 03:28:39 +0545

    fix dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['page.js', 'page.jsx', 'js', 'jsx'],
+  webpack: (config) => {
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false, path: false, os: false, pg: false, net: false, tls: false, dns: false };
+    return config;
+  },
   env: {
     VIMEO_ACCESS_TOKEN: process.env.VIMEO_ACCESS_TOKEN,
     VIMEO_USER_ID: process.env.VIMEO_USER_ID,
